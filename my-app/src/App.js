9a7d23abb749c733c16fec73e9415ed878c372c6
RaveTheN <peppereres@live.it>
2023-01-30 23:29:25 +0100

    props_3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,6 @@ import { Welcome } from "./Welcome";
 export class App extends Component {
 
     render(){
-        return <Welcome name="Johnny" />
-        //Senza alcun prop "name" passato al coomponente "Welcome", verrebbe renderizzato il contenuto
-    //all'interno del tag p del componente "Welcome", ma omettendo completamente tutto il contenuto di {},
-    //parentesi comprese. Non so se è possibile settare un valore di default per "name", ma credo che il valore che 
-    //assegniamo (ad esempio) in questo return, possa comunque essere cambiato in seguito.
+        return <Welcome name="Johnny" age="21"/>
     }
 }
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -2,6 +2,11 @@ import { Component } from "react";
 
 export class Welcome extends Component {
     render(){
-        return <p>Welcome, {this.props.name}!</p>
+        return (
+            <div>
+                <p>Welcome, {this.props.name}!</p>
+                <p>Your age is {this.props.age}.</p>
+            </div>
+        )
     }
 }
