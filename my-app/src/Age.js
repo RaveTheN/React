55c76016543e23d0c8c06086e19fda5a421abd97
RaveTheN <peppereres@live.it>
2023-01-30 23:44:51 +0100

    props_5

---
+++ my-app/src/Age.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export class Age extends Component {
+    render(){
+        return `Your age is ${this.props.age}.`
+    }
+}
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import { Age } from "./Age";
 
 export class Welcome extends Component {
     render(){
         return (
             <div>
                 <p>Welcome, {this.props.name}!</p>
-                <p>Your age is {this.props.age}.</p>
+                <p><Age age={this.props.age}/></p>
             </div>
         )
     }
