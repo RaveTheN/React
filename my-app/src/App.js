7fb1e1ebbeb3b2a7547e38b10b3bdbbc1f67cc46
RaveTheN <peppereres@live.it>
2023-01-30 23:22:59 +0100

    props_2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,5 +5,9 @@ export class App extends Component {
 
     render(){
         return <Welcome name="Johnny" />
+        //Senza alcun prop "name" passato al coomponente "Welcome", verrebbe renderizzato il contenuto
+    //all'interno del tag p del componente "Welcome", ma omettendo completamente tutto il contenuto di {},
+    //parentesi comprese. Non so se è possibile settare un valore di default per "name", ma credo che il valore che 
+    //assegniamo (ad esempio) in questo return, possa comunque essere cambiato in seguito.
     }
 }
