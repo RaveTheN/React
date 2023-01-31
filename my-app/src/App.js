82c8f47e9e297417bcfa97156f76fdb9ddf92bbd
RaveTheN <peppereres@live.it>
2023-01-31 20:04:21 +0100

    conditional_rendering_1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,6 @@ export class App extends Component {
     //Whenever a prop is not a string, it can be
     //passed by embedding an expression within curly brackets
     render(){
-        return <Welcome name={<strong>Johnny</strong>} age="21"/>
+        return <Welcome name={<strong>Johnny</strong>} age={19}/>
     }
 }
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends Component {
         return (
             <div>
                 <p>Welcome, {this.props.name}!</p>
-                <p><Age age={this.props.age}/></p>
+                {this.props.age>18 && <p><Age age={this.props.age}/></p>}
             </div>
         )
     }
