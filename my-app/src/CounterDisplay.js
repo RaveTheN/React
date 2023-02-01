1d9e3baa3c133954ff109c9fb267f9ff2c9dcf6f
RaveTheN <peppereres@live.it>
2023-02-01 13:24:38 +0100

    component_lifecycle

--- my-app/src/CounterDisplay.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-import { Counter } from "./Counter";
-
-export class CounterDisplay extends Component{
-    render(){
-        return <h1>Count: {this.props.count}</h1>
-    }
-}
